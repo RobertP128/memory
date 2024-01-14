61477bb093a280c50496ca3c7dd8b3def0d92fdc
Robert Poppinger <robert.poppinger@iotechnik.com>
2024-01-14 20:17:00 +0100

    add storing the session Appmodels in a dictionary

--- src/main/java/net/poppinger/memory/controller/APIController.java
+++ src/main/java/net/poppinger/memory/controller/APIController.java
@@ -8,27 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/")
 public class APIController {
 
 
-
+    private Map<String, AppModel> sessions = new HashMap<>();
 
     public APIController() {
     }
 
 
     private AppModel getAppModel(HttpSession session) {
-        var appmodel=(AppModel)session.getAttribute("appModel");
-        if (appmodel==null){
-            appmodel = new AppModel();
+        var appmodelKey=(String)session.getAttribute("appModel");
+        if (appmodelKey==null){
+            appmodelKey=session.getId();
+            var appmodel = new AppModel();
             setAppModel(session,appmodel);
         }
+        var appmodel = sessions.get(appmodelKey);
         return appmodel;
     }
     private void setAppModel(HttpSession session,AppModel appModel) {
-        session.setAttribute("appModel",appModel);
+        session.setAttribute("appModel",session.getId());
+        sessions.put(session.getId(),appModel);
     }
 
 
