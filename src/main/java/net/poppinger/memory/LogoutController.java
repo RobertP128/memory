e0b5cb4ed779c787ebb8250911f3ad6db968bd7d
Robert Poppinger <robert.poppinger@iotechnik.com>
2024-01-23 17:27:59 +0100

    added authorisation

---
+++ src/main/java/net/poppinger/memory/LogoutController.java
@@ -0,0 +1,20 @@
+package net.poppinger.memory;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogoutController {
+    @RequestMapping("/logout")
+    public String performLogout(Authentication authentication, HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, response, authentication);
+        return "redirect:/";
+    }
+}
