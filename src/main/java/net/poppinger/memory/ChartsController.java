f85fdaf825797a88606a7b5314af4be01d6b6739
Robert Poppinger <robert.poppinger@iotechnik.com>
2024-01-24 16:07:39 +0100

    test Kendoo charts

--- src/main/java/net/poppinger/memory/ChartsController.java
+++ src/main/java/net/poppinger/memory/ChartsController.java
@@ -6,22 +6,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/charts/")
 public class ChartsController {
 
     @RequestMapping("index.html")
-    public ModelAndView index() throws JsonProcessingException {
+    public ModelAndView index() throws JsonProcessingException, FileNotFoundException,IOException {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("charts/index.html");
 
-        int[][] chartData=new int[10][2];
+        // read data1.txt from resources folder
+        // get Resources Folder
+        File f=new File(ChartsController.class.getClassLoader().getResource("data1.txt").getFile());
+        InputStream in = new FileInputStream(f);
+        BufferedReader br=new BufferedReader(new InputStreamReader(in));
 
-        for (int x=0;x<chartData.length;x++){
-            for (int y=0;y<chartData[x].length;y++){
-                chartData[x][y]=x+y;
+        List<List<Float>> chartData=new ArrayList<>();
+
+        String line=br.readLine();
+        while((line=br.readLine())!=null) {
+            String[] parts=line.split("\t");
+            if (parts.length==2){
+                var x=Float.parseFloat(parts[0])/1024;
+                var y=Float.parseFloat(parts[1]);
+                chartData.add(new ArrayList<>(List.of(x,y)));
             }
+
         }
+
+
         ObjectMapper mapper = new ObjectMapper();
         modelAndView.addObject("chartData",mapper.writeValueAsString(chartData));
         return modelAndView;
